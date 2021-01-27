088c49606a1d90ac13c5690d0921238ebbeb7f03
mishalkazmi <mishalkazmi@sabanciuniv.edu>
2021-01-27 09:47:38 +0100

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,29 @@
+# 1. Find the mean of each list, save it in a new variable and print it
+
+list_1 = [5, 9, 3, 1, 2]
+
+# mean_list_1
+
+# print
+
+list_2 = [0, 8, 0, 0, 4, 6]
+
+# mean_list_2
+
+# print
+
+list_3 = [8, 2, 5, 9, 1]
+
+# mean_list_3
+
+# print
+
+# 2. Add the resulting means of the above lists to a new list for comparison and print this new list
+
+# comp_mean =
+
+# print
+
+# 3. Compare elements in the comparison list and print out the greater mean value
+
+# print
